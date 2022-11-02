3fc0912c0ce72f54e55051a3df876c5ae1db6830
Anna Wu <114878672+AnnaWXM@users.noreply.github.com>
2022-11-02 13:30:21 +0200

    modify ContactService

--- src/main/java/IDM/ContactService.java
+++ src/main/java/IDM/ContactService.java
@@ -4,7 +4,7 @@ package IDM;
 public class ContactService implements CSI{
 
     public void list() {
-        System.out.println("Print all emails and displaynames, I didn't figure out how to do it.");
+        System.out.println("Print all emails and displaynames.");
     }
     
 }
